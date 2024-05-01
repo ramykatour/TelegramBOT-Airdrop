d7742ab6e4674e3f3627c8b3f4c1ed524ced9961
Ramy Katour <ramymouner@hotmail.com>
2024-05-01 20:20:42 +0200

    upload

---
+++ config.js
@@ -0,0 +1,17 @@
+     // replace the value below with the Telegram token you receive from @BotFather
+     var token = "7150209260:AAHLhJmyZgWXtmXpR5hPQyLUG3ekm8KitVU";
+     // firebase config from console.firebase
+     // Your web app's Firebase configuration
+     var config = {
+         apiKey: "AIzaSyCaGNTUFLcXGtY0CGDZDIqwDdlPxm5jACw",
+         authDomain: "telegram-bot-f3f3d.firebaseapp.com",
+         databaseURL: "https://telegram-bot-f3f3d.firebaseio.com/",
+         projectId: "telegram-bot-f3f3d",
+         storageBucket: "telegram-bot-f3f3d.appspot.com",
+         messagingSenderId: "996806217748",
+         appId: "1:996806217748:web:c396c66dd9a981f0235701",
+         measurementId: "G-4RHLP92LZ",
+     };
+
+     exports.token = token;
+     exports.firebase_config = firebaseConfig;
